88052f51c5d4ed06959f4a46d4214718f1f362af
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-11-21 16:04:24 -0600

    Refactorizado cdogio keyInput

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -4,107 +4,81 @@ import java.awt.event.KeyEvent;
 
 public class KeyInput extends KeyAdapter {
 
-	
 	private Handler handler;
 	
-	private boolean uP = false;
-	private boolean dP = false;
-	private boolean lP = false;
-	private boolean rP = false;
+	private boolean uP, 
+					dP, 
+					lP, 
+					rP;
 	
 	public KeyInput(Handler handler){
 		this.handler = handler;
+		this.uP = this.dP = this.lP = this.rP = false;
 	}	
 	//Obtiene el codigo ASCII de la tecla
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();	
 		for(GameObject tempObject : this.handler.gameObjects) {		
 			if(tempObject.getId() == ID.Player){
-				//Eventos para el jugador 1				
-				if(key == KeyEvent.VK_W) 
-				{
-					uP = true;
-					tempObject.setVelY(-5);
-				}
-				if(key == KeyEvent.VK_S) 
-				{
-					dP = true;
-					tempObject.setVelY(5);
-				}
-				if(key == KeyEvent.VK_D) 
-				{
-					rP = true;
-					tempObject.setVelX(5);
-				}
-				if(key == KeyEvent.VK_A) 
-				{
-					lP = true;
-					tempObject.setVelX(-5);
+				switch(key) {
+					case KeyEvent.VK_W:
+						uP = true;
+						tempObject.setVelY(-5);
+						break;
+					case KeyEvent.VK_S:
+						dP = true;
+						tempObject.setVelY(5);
+						break;
+					case KeyEvent.VK_D:
+						rP = true;
+						tempObject.setVelX(5);
+						break;
+					case KeyEvent.VK_A:
+						lP = true;
+						tempObject.setVelX(-5);
+						break;
+					case KeyEvent.VK_ESCAPE:
+						System.exit(1);
+						break;
 				}
 			}
 		}
-		if(key == KeyEvent.VK_ESCAPE) System.exit(1);
 	}	
+	
+	
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
 		//Obtiene el valor ascci de la tecla soltada
 		//Si el bojeto es un jugador, se dejara de mover
 		for(GameObject tempObject : this.handler.gameObjects) {		
 			if(tempObject.getId() == ID.Player) {
-				//Eventos para el jugador 1	
-				if(key == KeyEvent.VK_W) 
-				{
-					uP = false;
-					if(dP) 
-					{
-						tempObject.setVelY(5);
-					}
-					else 
-					{
-						tempObject.setVelY(0);
-					}
-				}
+				switch(key) {
 				
-					if(key == KeyEvent.VK_S) 
-				{
-						dP = false;
-						if(uP) 
-						{
-							tempObject.setVelY(-5);
-						}
-						else 
-						{
-							tempObject.setVelY(0);
-						}
-				}
-					
-					if(key == KeyEvent.VK_D) 
-				{
+					case KeyEvent.VK_W:
+						uP = false;
+						if(dP) { tempObject.setVelY(5);}
+						else {tempObject.setVelY(0);}
+						break;
+						
+					case KeyEvent.VK_S: 
+							dP = false;
+							if(uP) {tempObject.setVelY(-5);}
+							else {tempObject.setVelY(0);}
+							break;
+		
+					case KeyEvent.VK_D:
 						rP = false;
-						if(lP) 
-						{
-							tempObject.setVelX(-5);
-						}
-						else 
-						{
-							tempObject.setVelX(0);
-						}
-				}
-					
-					if(key == KeyEvent.VK_A)
-				{
-						lP = false;
-						if(rP) 
-						{
-							tempObject.setVelX(5);
-						}
-						else 
-						{
-							tempObject.setVelX(0);
-						}
-				}
+						if(lP) {tempObject.setVelX(-5);}
+						else {tempObject.setVelX(0);}
+						break;
+						
+					case KeyEvent.VK_A:
+							lP = false;
+							if(rP) {tempObject.setVelX(5);}
+							else {tempObject.setVelX(0);}
+							break;
+				}		
 			}
-			
 		}
 	}
 	
